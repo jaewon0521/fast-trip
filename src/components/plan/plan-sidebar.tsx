8b14ca0e39cb6cb496ab2df4fde95a1075326672
jaewon <yjw970521@gmail.com>
2025-07-21 23:05:54 +0900

    Refactor : 일자별 버튼 동적 스타일 적용

--- src/components/plan/plan-sidebar.tsx
+++ src/components/plan/plan-sidebar.tsx
@@ -2,12 +2,17 @@
 
 import { PlaceResult } from "@/service/google/places-dto";
 import PlanSchedule from "./plan-schedule";
+import { Dispatch, SetStateAction } from "react";
+import { cn } from "@/lib/utils";
+import PlaceDayButton from "./place-day-button";
 
 interface PlanSidebarProps {
   places: PlaceResult[];
   daysCount: number;
   markers: PlaceResult[][];
   onTogglePlace: (day: number, place: PlaceResult) => void;
+  selectedDay: number | null;
+  setSelectedDay: Dispatch<SetStateAction<number | null>>;
 }
 
 export default function PlanSidebar({
@@ -15,11 +20,31 @@ export default function PlanSidebar({
   daysCount,
   markers,
   onTogglePlace,
+  selectedDay,
+  setSelectedDay,
 }: PlanSidebarProps) {
   return (
     <>
       <section className="px-6 py-4 border-b border-gray-100">
-        <div className="text-sm text-gray-600">장소 선택</div>
+        <div className="flex gap-2 mt-2 overflow-x-auto">
+          <PlaceDayButton
+            dayNumber={0}
+            isSelected={selectedDay === null}
+            onClick={() => setSelectedDay(null)}
+          >
+            전체
+          </PlaceDayButton>
+          {Array.from({ length: daysCount }, (_, index) => (
+            <PlaceDayButton
+              key={index}
+              dayNumber={index + 1}
+              isSelected={selectedDay === index}
+              onClick={() => setSelectedDay(index)}
+            >
+              {index + 1}일차
+            </PlaceDayButton>
+          ))}
+        </div>
       </section>
       <nav className="flex-1 px-4 py-2">
         {Array.from({ length: daysCount }, (_, index) => (
