8b247967b9e59af622a52c33cce1c19a9637930f
jaewon <yjw970521@gmail.com>
2025-07-31 00:20:17 +0900

    Chore : remove key

--- src/components/question/question-item.tsx
+++ src/components/question/question-item.tsx
@@ -14,7 +14,7 @@ export function QuestionItem({
   const isSelected = selectedContent === content;
 
   return (
-    <li key={content}>
+    <li>
       <div>
         <button
           type="button"
