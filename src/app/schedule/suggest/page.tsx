350db55a644da057b6c220dff0d09bb5427a6008
jaewon <yjw970521@gmail.com>
2025-07-31 11:31:38 +0900

    Chore : Remove Not Used Valiant

--- src/app/schedule/suggest/page.tsx
+++ src/app/schedule/suggest/page.tsx
@@ -7,7 +7,6 @@ import QuestionCity from "@/components/question/question-city";
 import QuestionDays from "@/components/question/question-days";
 import QuestionCompanion from "@/components/question/question-companion";
 import QuestionTravelType from "@/components/question/question-travel-type";
-import { ArrowLeftIcon } from "lucide-react";
 
 type CompanionPlanStep = {
   region: string;
@@ -41,7 +40,6 @@ export default function SuggestPage() {
 
   const stepCurrent = Funnel.index + 1;
   const stepTotal = 4;
-
 
   return (
     <Funnel.Render
