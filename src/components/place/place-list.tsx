4297fc8d9fc5a8ec10588edf8e3f76c26debee58
jaewon <yjw970521@gmail.com>
2025-08-03 22:08:38 +0900

    Fix : typo, key name

--- src/components/place/place-list.tsx
+++ src/components/place/place-list.tsx
@@ -6,15 +6,12 @@ interface PlaceListProps {
   onDeletePlace: (place: PlaceResult) => void;
 }
 
-export default function PlaceList({
-  places,
-  onDeletePlace,
-}: PlaceListProps) {
+export default function PlaceList({ places, onDeletePlace }: PlaceListProps) {
   return (
     <ul className="space-y-4">
       {places.map((place, index) => (
         <PlaceItem
-          key={place.place_id}
+          key={`${place.place_id}-${index}`}
           place={place}
           number={index + 1}
           onDeletePlace={onDeletePlace}
