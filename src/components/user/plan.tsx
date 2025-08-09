acf76ee403d520459f44fbbb7ee77b7db17a57b9
jaewon <yjw970521@gmail.com>
2025-08-09 23:36:58 +0900

    Style : 내 여행 목록 UI 수정

--- src/components/user/plan.tsx
+++ src/components/user/plan.tsx
@@ -11,12 +11,14 @@ export default function UserPlan({ plans }: UserPlanProps) {
     <Responsive>
       <div className="pt-24 pb-10 px-8">
         <div className="flex w-full mb-10">
-          <div className="flex w-full border-b-4 border-blue-500 pb-4 text-2xl text-blue-500 gap-2">
-            <span>나의 일정</span>
-            <span className="font-bold">{plans.length}</span>
-          </div>
+          <h1 className="flex w-full items-center border-b-4 border-blue-500 pb-4 text-blue-500 gap-2">
+            <span className="text-xl">나의 일정</span>
+            <span className="text-xl font-bold">{plans.length}</span>
+          </h1>
         </div>
-        <div>나의 여행 도시 목록</div>
+        <div className="mb-7">
+          <span className="text-2xl font-semibold">나의 여행 도시 목록</span>
+        </div>
         <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-5">
           {plans.map((plan, idx) => (
             <PlanCardItem key={`${plan.id}_${idx}`} plan={plan} />
