320b061b8c0d5f9734a692b29ddf014bce595cc3
jaewon0521 <yjw970521@gmail.com>
2025-08-14 15:50:27 +0900

    Feat : PWA 설정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,25 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // 개발환경에서는 비활성화
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "offlineCache",
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
@@ -16,4 +35,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default withBundleAnalyzer(withPWA(nextConfig));
